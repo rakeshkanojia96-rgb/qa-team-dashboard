98826e37fdc3ad332cefe03bb79402891396eec8
rakeshkanojia96-rgb <rakesh@Rakeshs-MacBook-Air.local>
2025-12-18 19:09:02 +0530

    Add 'Statutory Holiday - India' option to holiday types

--- app.js
+++ app.js
@@ -4191,7 +4191,8 @@ function renderHolidaysList() {
         const typeColors = {
             'public': 'bg-red-100 text-red-800',
             'company': 'bg-blue-100 text-blue-800',
-            'optional': 'bg-green-100 text-green-800'
+            'optional': 'bg-green-100 text-green-800',
+            'statutory-india': 'bg-orange-100 text-orange-800'
         };
         const typeColor = typeColors[holiday.type] || 'bg-gray-100 text-gray-800';
         
